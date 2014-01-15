d258eef2b4d6fc9e898d37811d2ec43e8f335318
George MacRorie <george.macrorie@semantico.com>
2014-01-15 18:29:22 +0000

    Initial bare rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeorgemacVanillaRails::Application.config.secret_key_base = '0ed6aca5d048769f28028994511e36fb1059d39875a4858cc67fadbd51fd116041fd644406a0d00e7a09883c893f0911764eb5005af92895ce68ff24c7d7e5e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeorgemacVanillaRails::Application.config.session_store :cookie_store, key: '_georgemac-vanilla-rails_session'
